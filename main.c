a1728da37a2c9c91bc27f33694ea06047152ecbd
hkndrsn <75948987+hkndrsn@users.noreply.github.com>
2020-12-16 23:30:29 +0300

    Printf Eklendi(2)

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 
-
 int topla(int x, int y){
 
 	return x + y;
@@ -9,12 +8,7 @@ int topla(int x, int y){
 
 int main(){
 	
-	printf("%d",topla(5,6));
-	
-	
-	
+	printf("%d",topla(5,6));	
+	printf("%d",topla(10,6));
 	return 0;
 }
-
-
-
